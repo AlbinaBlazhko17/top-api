5bd6e1f756542eab51ce584bfa93be01dd2caa26
Albinator <albinamivina@gmail.com>
2023-05-15 11:17:02 +0300

    add function to find user by email

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,18 +1,27 @@
 import { Injectable } from '@nestjs/common';
 import { AuthDto } from './dto/auth.dto';
 import { UserModel } from './user.model';
-import { genSaltSync } from 'bcryptjs';
+import { genSaltSync, hashSync } from 'bcryptjs';
 import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class AuthService {
 	constructor(
-		@InjectModel(UserModel.name) private readonly userModel: UserModel,
+		@InjectModel(UserModel.name)
+		private readonly userModel: Model<UserModel>,
 	) {}
 
 	async createUser(dto: AuthDto) {
 		const salt = genSaltSync(10);
+		const newUser = new this.userModel({
+			email: dto.login,
+			passwordHash: hashSync(dto.password, salt),
+		});
+		return newUser.save();
 	}
 
-	async findUser(email: string) {}
+	async findUser(email: string) {
+		return this.userModel.findOne({ email }).exec();
+	}
 }
