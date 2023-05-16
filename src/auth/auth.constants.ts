bcb92caed56d7e122a7f82fa53db2b775e5f06e5
Albinator <albinamivina@gmail.com>
2023-05-16 09:57:31 +0300

    authorization and generate jwt

--- src/auth/auth.constants.ts
+++ src/auth/auth.constants.ts
@@ -1,1 +1,3 @@
 export const ALREADY_REGISTERED_ERROR = 'This user is already exist';
+export const USER_NOT_FOUND_ERROR = 'There is no user with such email';
+export const WRONG_PASSWORD_ERROR = 'Password is incorrect';
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,6 +3,9 @@ import { AuthController } from './auth.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserModel, UserModelSchema } from './user.model';
 import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { getJWTConfig } from 'src/configs/jwt.config';
 
 @Module({
 	controllers: [AuthController],
@@ -10,6 +13,11 @@ import { AuthService } from './auth.service';
 		MongooseModule.forFeature([
 			{ name: UserModel.name, schema: UserModelSchema },
 		]),
+		JwtModule.registerAsync({
+			imports: [ConfigModule],
+			inject: [ConfigService],
+			useFactory: getJWTConfig,
+		}),
 	],
 	providers: [AuthService],
 })
