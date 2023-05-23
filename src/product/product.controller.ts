b59334e37decc3c5ce1eaf23be9309754af26fad
Albinator <albinamivina@gmail.com>
2023-05-23 11:19:05 +0300

    add uathGuards to product

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -8,6 +8,7 @@ import {
 	Param,
 	Patch,
 	Post,
+	UseGuards,
 	UsePipes,
 	ValidationPipe,
 } from '@nestjs/common';
@@ -17,16 +18,19 @@ import { CreateProductDto } from './dto/create-product.dto';
 import { ProductService } from './product.service';
 import { PRODUCT_NOT_FOUND_ERROR } from './product.constants';
 import { IdValidationPipe } from '../pipe/id-validation.pipe';
+import { JwtAuthGuard } from 'src/auth/guards/jwt.guard';
 
 @Controller('product')
 export class ProductController {
 	constructor(private readonly productService: ProductService) {}
 
+	@UseGuards(JwtAuthGuard)
 	@Post('create')
 	async create(@Body() dto: CreateProductDto): Promise<ProductModel> {
 		return this.productService.create(dto);
 	}
 
+	@UseGuards(JwtAuthGuard)
 	@Get(':id')
 	async get(@Param('id') id: string): Promise<ProductModel> {
 		const product = await this.productService.findById(id);
@@ -34,6 +38,7 @@ export class ProductController {
 		return product;
 	}
 
+	@UseGuards(JwtAuthGuard)
 	@Delete(':id')
 	async delete(@Param('id', IdValidationPipe) id: string): Promise<void> {
 		const deletedProduct = await this.productService.deleteById(id);
@@ -41,6 +46,7 @@ export class ProductController {
 			throw new NotFoundException(PRODUCT_NOT_FOUND_ERROR);
 	}
 
+	@UseGuards(JwtAuthGuard)
 	@Patch(':id')
 	async patch(
 		@Param('id', IdValidationPipe) id: string,
